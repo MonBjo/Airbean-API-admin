4f9e2ef1355a9e710279aa9c11770470527e37c4
Monica Björk <m.bjork95@gmail.com>
2022-06-17 12:39:10 +0200

    Moved validKeys array to its own file

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,22 +1,13 @@
 function auth(req, res, next) {
-  const APIkeys = [
-    "ZyPlY1LaR20qH3W41r16",
-    "uXrYAh42AR0rbKbAQ7Q0",
-    "lT5aT0QO356jG3IRHwJb",
-    "Owi0G2kUd6G9NJQlbvgg",
-    "Y9lA9mxx6eXqhYHLRPCS",
-    "rsm3OXDMa4bMY40SD7Vf"
-  ];  
-
+const validKeys = require('./validKeys.json').validKeys;
   const apiKey = req.headers['key'];
   const resObj = {
     success: "false",
     message: "You need an API key"
   }
-
   
   if(apiKey) {
-    if (APIkeys.includes(apiKey)) {
+    if (validKeys.includes(apiKey)) {
       next();
     } else {
       resObj.message = "API key is invalid"
